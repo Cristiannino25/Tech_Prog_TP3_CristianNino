455762b7c6d3fc8097d7f5bbbdd1ff31ef6cc603
Cristian Nino <cristiannino25@gmail.com>
2024-09-02 13:37:45 -0400

    modifier le cache v19

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-const CACHE_NAME = "static-cache-v18";
+///Update cache names any time any of the cached files change.
+const CACHE_NAME = "static-cache-v19";
 
+//Add list of files to cache here.
 const FILES_TO_CACHE = [
   "offline.html",
   "style/css/style.css",
   "medias/images/offline.webp",
 ];
 
+// INSTALLATION
 self.addEventListener("install", (evt) => {
   console.log("[ServiceWorker] Install");
+  // MISE EN CACHE
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("[ServiceWorker] Pre-caching offline page");
@@ -17,8 +21,10 @@ self.addEventListener("install", (evt) => {
   self.skipWaiting();
 });
 
+//ACTIVATION
 self.addEventListener("activate", (evt) => {
   console.log("[ServiceWorker] Activate");
+  //Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
@@ -33,17 +39,22 @@ self.addEventListener("activate", (evt) => {
   );
   self.clients.claim();
 });
-
+//ACCÈS À UNE RESSOURCE
 self.addEventListener("fetch", (evt) => {
   console.log("[ServiceWorker] Fetch", evt.request.url);
+  //Gestion de l'évènement fetch
   if (evt.request.mode !== "navigate") {
+    // Not a page navigation, bail.
     return;
   }
   evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("/offline.html");
-      });
-    })
+    fetch(evt.request) // on tente de récupérer la ressource
+      .catch(() => {
+        return caches
+          .open(CACHE_NAME) // si échec , on renvoie la page offlione
+          .then((cache) => {
+            return cache.match("offline.html ");
+          });
+      })
   );
 });
