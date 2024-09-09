4fde50e05c079ac1f33aaad8e506e0fc1d34602f
Cristian Nino <cristiannino25@gmail.com>
2024-09-09 19:47:02 -0400

    chache v31

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v30";
+const CACHE_NAME = "static-cache-v31";
 
 const FILES_TO_CACHE = [
   "/offline.html",
@@ -9,19 +9,17 @@ const FILES_TO_CACHE = [
   "https://cdn.tailwindcss.com",
 ];
 
-// Installation du service worker
 self.addEventListener("install", (evt) => {
   console.log("[ServiceWorker] Install");
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("[ServiceWorker] Pre-caching offline page and other assets");
+      console.log("[ServiceWorker] Pre-caching offline page");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-// Activation du service worker
 self.addEventListener("activate", (evt) => {
   console.log("[ServiceWorker] Activate");
   evt.waitUntil(
@@ -39,41 +37,14 @@ self.addEventListener("activate", (evt) => {
   self.clients.claim();
 });
 
-// Gestion des requêtes
 self.addEventListener("fetch", (evt) => {
   console.log("[ServiceWorker] Fetch", evt.request.url);
 
-  // Gestion des requêtes pour les pages HTML (naviguer)
   if (evt.request.mode === "navigate") {
     evt.respondWith(
       fetch(evt.request).catch(() => {
-        return caches.match("/offline.html");
+        return caches.match("offline.html");
       })
-    );
-  } else {
-    // Pour les autres requêtes (CSS, JS, images, etc.)
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then((response) => {
-          return (
-            response ||
-            fetch(evt.request).then((networkResponse) => {
-              if (networkResponse && networkResponse.status === 200) {
-                let responseClone = networkResponse.clone();
-                caches.open(CACHE_NAME).then((cache) => {
-                  cache.put(evt.request, responseClone);
-                });
-              }
-              return networkResponse;
-            })
-          );
-        })
-        .catch(() => {
-          if (evt.request.mode === "navigate") {
-            return caches.match("/offline.html");
-          }
-        })
     );
   }
 });
